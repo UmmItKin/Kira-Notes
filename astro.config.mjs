2aead846aab16e9a4acf72d811eb37228b5b7933
UmmIt <git@ummit.dev>
2026-07-01 18:11:08 +0800

    feat: add unocss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import UnoCSS from '@unocss/astro';
 
 import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+	integrations: [vue(), UnoCSS()]
 });
